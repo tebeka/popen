84a3ed322f223986d440de368d1d500bd4d7e4ff
Miki Tebeka <miki.tebeka@gmail.com>
2011-02-14 17:18:38 -0800

    2 more tests

--- test/popen_test.clj
+++ test/popen_test.clj
@@ -1,6 +1,7 @@
 (ns popen-test
   (:use [popen] :reload-all)
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:require [clojure.java.io :as io]))
 
 (deftest popen-test
   (let [p (popen/popen ["ls" "/tmp"])]
@@ -10,4 +11,22 @@
     (is (zero? (popen/join p)))
     (is (zero? (popen/exit-code p)))
     (is (nil? (popen/kill p)))))
+
+(deftest read-test
+  (let [p (popen/popen ["cat"])
+        out (popen/stdout p)
+        in (popen/stdin p)]
+    (binding [*out* in]
+      (println 1)
+      (println 2))
+    (.close in)
+    (is (zero? (popen/exit-code p)))
+    (is (= (line-seq (io/reader out)) '("1" "2")))))
+
+(deftest run-and-kill-test
+  (let [p (popen/popen ["cat"])]
+    (is (popen/running? p))
+    (popen/kill p)
+    (is (not (running? p)))
+    (is (not (zero? (exit-code p))))))
 
