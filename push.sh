d1433db6af11c4554867d45436df27240d60c3bb
Miki Tebeka <miki.tebeka@gmail.com>
2011-02-14 17:26:05 -0800

    Push to clojars

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+version=$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?" project.clj  | head -1)
+jar=popen-${version}.jar
+
+if [ -f $jar ]; then
+    rm $jar
+fi
+lein jar
+lein pom
+scp pom.xml $jar clojars@clojars.org:
