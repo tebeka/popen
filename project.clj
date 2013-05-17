55e7540cc1c868a3bbb0556807a943541cdc3b34
Miki Tebeka <miki.tebeka@gmail.com>
2013-05-17 16:53:44 -0700

    0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject popen "0.2.0"
+(defproject popen "0.3.0"
   :description "popen (subprocess) library for Clojure"
   :url "https://bitbucket.org/tebeka/popen/src"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
